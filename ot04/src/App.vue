dcdc72abccc4cd3fca508adb0db94b1f2c023b1a
lucas-travassos <lucasstravassos19@gmail.com>
2026-06-25 21:44:03 -0300

    feat (ot04): desenvolve pratica1 - layout com grid system

--- ot04/src/App.vue
+++ ot04/src/App.vue
@@ -1,11 +1,28 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="4">
+            <v-card>
+              <v-card-title>Coluna 1</v-card-title>
+              <v-card-text>Conteudo da primeira coluna.</v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="12" md="4">
+            <v-card>
+              <v-card-title>Coluna 2</v-card-title>
+              <v-card-text>Conteudo da segunda coluna.</v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="12" md="4">
+            <v-card>
+              <v-card-title>Coluna 3</v-card-title>
+              <v-card-text>Conteudo da terceira coluna.</v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
-
-<style scoped></style>
